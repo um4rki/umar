f1c3d5b72ee0079fec7757c5da583d145c784632
umar <qarshiyevumar@gmail.com>
2024-09-24 11:11:42 +0500

    Student Serviceni gitga commit qildim

---
+++ StudentService.java
@@ -0,0 +1,34 @@
+package Lesson_9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by: Umar
+ * DateTime: 9/24/2024 10:56 AM
+ */
+public class StudentService {
+    private static List<Student> students = new ArrayList<>();
+
+    public void add(Student student) {
+        students.add(student);
+        System.out.println("Student added " + student);
+    }
+
+    public void updateStudent(String newName, int newAge){
+        for (Student student : students) {
+            if(student.getFirstName().equals(newName) && student.getAge() == newAge){
+                student.setFirstName(newName);
+                student.setAge(newAge);
+                System.out.println("Student updated " + student);
+                return;
+            }
+        }
+        System.out.println("Student topilmadi");
+    }
+
+    public void deleteStudent(String studentName){
+        students.removeIf(student -> student.getFirstName().equals(studentName));
+        System.out.println("Student deleted " + studentName);
+    }
+}
